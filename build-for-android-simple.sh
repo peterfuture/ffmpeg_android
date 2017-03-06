0a01983a0a1a9bbcdd1e0e8365558f00d6abd73c
peter-s <peter_future@outlook.com>
2017-03-06 18:02:04 +0800

    [BUILD] add simplify build script

---
+++ build-for-android-simple.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+DEST=`pwd`/build/ffmpeg && rm -rf $DEST
+SOURCE=`pwd`/ffmpeg
+
+if [ -d ffmpeg ]; then
+  cd ffmpeg
+else
+  git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
+  cd ffmpeg
+fi
+
+git reset --hard
+git clean -f -d
+#git checkout `cat ../ffmpeg-version`
+[ $PIPESTATUS == 0 ] || exit 1
+
+git log --pretty=format:%H -1 > ../ffmpeg-version
+
+TOOLCHAIN=/tmp/vplayer
+SYSROOT=$TOOLCHAIN/sysroot/
+$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --platform=android-19 --install-dir=$TOOLCHAIN
+
+export PATH=$TOOLCHAIN/bin:$PATH
+export CC="ccache arm-linux-androideabi-gcc"
+export LD=arm-linux-androideabi-ld
+export AR=arm-linux-androideabi-ar
+
+CFLAGS="-O3 -Wall -mthumb -pipe -fpie -fasm \
+  -finline-limit=300 -ffast-math \
+  -fstrict-aliasing -Werror=strict-aliasing \
+  -fmodulo-sched -fmodulo-sched-allow-regmoves \
+  -Wno-psabi -Wa,--noexecstack \
+  -D__ARM_ARCH_5__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5TE__ \
+  -DANDROID -DNDEBUG"
+
+FFMPEG_FLAGS="--target-os=linux \
+  --arch=arm \
+  --enable-cross-compile \
+  --cross-prefix=arm-linux-androideabi- \
+  --enable-shared \
+  --enable-static \
+  --enable-jni \
+  --enable-mediacodec \
+  --disable-everything \
+  --enable-decoder=h264 \
+  --enable-decoder=h264_mediacodec \
+  --enable-decoder=aac \
+  --enable-decoder=mp3 \
+  --enable-demuxer=mpegts \
+  --enable-protocol=file \
+  --enable-version3"
+
+#for version in neon armv7 vfp armv6; do
+for version in armv7; do
+
+  cd $SOURCE
+
+  case $version in
+    neon)
+      EXTRA_CFLAGS="-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mvectorize-with-neon-quad"
+      EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
+      ;;
+    armv7)
+      EXTRA_CFLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
+      EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
+      ;;
+    vfp)
+      EXTRA_CFLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=softfp"
+      EXTRA_LDFLAGS=""
+      ;;
+    armv6)
+      EXTRA_CFLAGS="-march=armv6"
+      EXTRA_LDFLAGS=""
+      ;;
+    *)
+      EXTRA_CFLAGS=""
+      EXTRA_LDFLAGS=""
+      ;;
+  esac
+
+  PREFIX="$DEST/$version" && mkdir -p $PREFIX
+  FFMPEG_FLAGS="$FFMPEG_FLAGS --prefix=$PREFIX"
+
+  ./configure $FFMPEG_FLAGS --extra-cflags="$CFLAGS $EXTRA_CFLAGS" --extra-ldflags="$EXTRA_LDFLAGS" | tee $PREFIX/configuration.txt
+  cp config.* $PREFIX
+  [ $PIPESTATUS == 0 ] || exit 1
+
+  make clean
+  make -j32 || exit 1
+  make install || exit 1
+
+  #$AR rcs $PREFIX/libffmpeg.a libavutil/*.o libavcodec/*.o libavformat/*.o libswresample/*.o libswscale/*.o  
+  #arm-linux-androideabi-strip --strip-unneeded $PREFIX/libffmpeg.a
+
+done
