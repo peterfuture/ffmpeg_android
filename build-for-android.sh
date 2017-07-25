966106cd553a6b1990d0ead307246b980057ce60
peter-s <peter_future@outlook.com>
2017-07-25 20:28:59 +0800

    [config] remove unused ffmpeg items

--- build-for-android.sh
+++ build-for-android.sh
@@ -45,6 +45,7 @@ CFLAGS="-O3 -Wall -pipe \
     -DANDROID -DNDEBUG"
 
 FFMPEG_FLAGS="
+  --disable-debug \
   --enable-shared \
   --enable-pthreads \
   --disable-symver \
@@ -57,17 +58,122 @@ FFMPEG_FLAGS="
   --disable-encoders \
   --disable-muxers \
   --disable-devices \
+  --disable-indevs \
+  --disable-outdevs \
   --enable-protocols  \
   --enable-parsers \
-  --enable-demuxers \
-  --enable-decoders \
+  --disable-demuxers \
+  --disable-decoders \
   --enable-bsfs \
   --enable-jni \
   --enable-mediacodec \
   --enable-network \
   --enable-swscale  \
-  --disable-demuxer=sbg \
   --disable-asm \
+
+  --enable-demuxer=aac \
+  --enable-demuxer=ac3 \
+  --enable-demuxer=amr \
+  --enable-demuxer=ape \
+  --enable-demuxer=asf \
+  --enable-demuxer=avs \
+  --enable-demuxer=cavsvideo \
+  --enable-demuxer=dts \
+  --enable-demuxer=dvbsub \
+  --enable-demuxer=dvbtxt \
+  --enable-demuxer=eac3 \
+  --enable-demuxer=flac \
+  --enable-demuxer=flv \
+  --enable-demuxer=g722 \
+  --enable-demuxer=g729 \
+  --enable-demuxer=h261 \
+  --enable-demuxer=h263 \
+  --enable-demuxer=h264 \
+  --enable-demuxer=hevc \
+  --enable-demuxer=hls \
+  --enable-demuxer=m4v \
+  --enable-demuxer=matroska \
+  --enable-demuxer=mjpeg \
+  --enable-demuxer=mjpeg_2000 \
+  --enable-demuxer=mov \
+  --enable-demuxer=mp3 \
+  --enable-demuxer=mpegps \
+  --enable-demuxer=mpegts \
+  --enable-demuxer=mpegtsraw \
+  --enable-demuxer=mpegvideo \
+  --enable-demuxer=mpjpeg \
+  --enable-demuxer=pcm_alaw \
+  --enable-demuxer=pcm_f32be \
+  --enable-demuxer=pcm_f32le \
+  --enable-demuxer=pcm_f64be \
+  --enable-demuxer=pcm_f64le \
+  --enable-demuxer=pcm_mulaw \
+  --enable-demuxer=pcm_s16be \
+  --enable-demuxer=pcm_s16le \
+  --enable-demuxer=pcm_s24be \
+  --enable-demuxer=pcm_s24le \
+  --enable-demuxer=pcm_s32be \
+  --enable-demuxer=pcm_s32le \
+  --enable-demuxer=pcm_s8 \
+  --enable-demuxer=pcm_u16be \
+  --enable-demuxer=pcm_u16le \
+  --enable-demuxer=pcm_u24be \
+  --enable-demuxer=pcm_u24le \
+  --enable-demuxer=pcm_u32be \
+  --enable-demuxer=pcm_u32le \
+  --enable-demuxer=pcm_u8 \
+  --enable-demuxer=rawvideo \
+  --enable-demuxer=rm \
+  --enable-demuxer=rtp \
+  --enable-demuxer=rtsp \
+  --enable-demuxer=vc1 \
+  --enable-demuxer=vobsub \
+  --enable-demuxer=wav \
+  --enable-demuxer=webvtt \
+  --enable-decoder=aac \
+  --enable-decoder=aac_latm \
+  --enable-decoder=ac3 \
+  --enable-decoder=alac \
+  --enable-decoder=amrnb \
+  --enable-decoder=amrwb \
+  --enable-decoder=ass \
+  --enable-decoder=truehd \
+  --enable-decoder=cavs \
+  --enable-decoder=dnx \
+  --enable-decoder=dnxhd \
+  --enable-decoder=cook \
+  --enable-decoder=dvbsub \
+  --enable-decoder=dvdsub \
+  --enable-decoder=eac3 \
+  --enable-decoder=vc1 \
+  --enable-decoder=flac \
+  --enable-decoder=flv \
+  --enable-decoder=g729 \
+  --enable-decoder=h261 \
+  --enable-decoder=h263 \
+  --enable-decoder=h264 \
+  --enable-decoder=hevc \
+  --enable-decoder=vp8 \
+  --enable-decoder=vp8_mediacodec \
+  --enable-decoder=h264_mediacodec \
+  --enable-decoder=hevc_mediacodec \
+  --enable-decoder=mjpeg \
+  --enable-decoder=mp2 \
+  --enable-decoder=mp3 \
+  --enable-decoder=mpeg2video \
+  --enable-decoder=mpeg4 \
+  --enable-decoder=mpegvideo \
+  --enable-decoder=vp9 \
+  --enable-decoder=vp9_mediacodec \
+  --enable-decoder=webvtt \
+  --enable-decoder=wmalossless \
+  --enable-decoder=wmapro \
+  --enable-decoder=pgssub \
+  --enable-decoder=rv10 \
+  --enable-decoder=rv20 \
+  --enable-decoder=rv30 \
+  --enable-decoder=rv40 \
+  --enable-decoder=yuv4 \
   --enable-version3"
 
 
